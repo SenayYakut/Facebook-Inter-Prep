2d563bdba7854076f07429f2bc7d3a69f2d8f05e
Senay Yakut <senaykt@gmail.com>
2019-09-23 15:02:10 -0700

    Fibonacci function

--- Concepts/recursive.js
+++ Concepts/recursive.js
@@ -19,3 +19,17 @@ function iterateAddToN(n){
     return sum;
 }
 iterateAddToN(10);
+
+//Task:Complete the fibonnacci function to return the N th  term
+
+
+var fib = function(num){
+	if(num === 1){
+		return 0;
+	}
+	if(num === 2){
+		return 1;
+	}
+	return fib(num-1)+fib(num-2);
+};
+console.log(fib(3));//1
