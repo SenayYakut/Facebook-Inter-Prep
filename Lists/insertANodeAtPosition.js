5cae8a2736569e7c1360e410741bfb4c5f86b408
Senay Yakut <senaykt@gmail.com>
2019-10-02 11:04:43 -0700

    more practice

--- Lists/insertANodeAtPosition.js
+++ Lists/insertANodeAtPosition.js
@@ -18,4 +18,29 @@ class Node{
     }
     this.size++;
     }
+    insertAt(head,element,position){
+        if(position>0 && position>this.size)
+        return false;
+        else{
+            var node =new Node(element);
+            var curr, prev;
+
+            curr = this.head;
+            if(index===0){
+                node.next=head;
+                this.head=node;
+            }else{
+                curr=this.head;
+                var it=0;
+                while(it<position){
+                    it++;
+                    prev=curr;
+                    curr=curr.next;
+                }
+                node.next=curr;
+                prev.next=node;
+            }
+            this.size++;
+        }
+    }
 }
