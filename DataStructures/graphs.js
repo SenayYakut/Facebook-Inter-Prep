6396c3518f678b297de7a74d84528ce41b952617
Senay Yakut <senaykt@gmail.com>
2019-10-11 15:10:29 -0700

    Graphs Depth-Firts-Search Breadth-First-Search

--- DataStructures/graphs.js
+++ DataStructures/graphs.js
@@ -6,7 +6,37 @@ class Graph{
 
     }
 }
+
 addVertex(v){
     this.AdjList.set(v,[]);
-
 }
+
+addEdge(v, w){
+    this.AdjList.get(v).push(w);
+    thisAdjListget(w).push(v);
+}
+
+printGraph(){
+    
+	// get all the vertices 
+	var get_keys = this.AdjList.keys(); 
+
+	// iterate over the vertices 
+	for (var i of get_keys){ 
+		// great the corresponding adjacency list 
+		// for the vertex 
+		var get_values = this.AdjList.get(i); 
+		var conc = ""; 
+
+		// iterate over the adjacency list 
+		// concatenate the values into a string 
+		for (var j of get_values) 
+			conc += j + " "; 
+
+		// print the vertex and its adjacency list 
+		console.log(i + " -> " + conc); 
+	} 
+} 
+// Edge Lists, Adjacency Matrices, Adjacency Lists.
+
+
